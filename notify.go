43866da399c3a76d70370f0fa76c4f8d9601b57b
Pierre Marchand <pierremarc07@gmail.com>
2018-03-30 12:28:21 +0200

    fixed notifications in a mess

--- notify.go
+++ notify.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"log"
+
+	"github.com/satori/go.uuid"
+)
+
 type Receiver func(Notification)
 
 type Notification struct {
@@ -8,23 +14,28 @@ type Notification struct {
 	Parent int    `json:"parent"`
 }
 
+type sucscription struct {
+	id uuid.UUID
+	fn Receiver
+}
+
 func MakeNotification(t string, r int, p int) Notification {
 	return Notification{t, r, p}
 }
 
 type Notifier struct {
 	source  chan Notification
-	clients []Receiver
+	clients []sucscription
 }
 
 func NewNotifier() *Notifier {
-	clients := make([]Receiver, 0)
+	clients := make([]sucscription, 0)
 	source := make(chan Notification)
 	n := Notifier{source, clients}
 	go func() {
 		for i := range source {
-			for _, r := range n.clients {
-				r(i)
+			for _, s := range n.clients {
+				s.fn(i)
 			}
 		}
 	}()
@@ -36,19 +47,20 @@ func (b *Notifier) Notify(n Notification) {
 	b.source <- n
 }
 
-func (b *Notifier) Subscribe(r Receiver) int {
-	b.clients = append(b.clients, r)
-	return len(b.clients) - 1
+func (b *Notifier) Subscribe(r Receiver) uuid.UUID {
+	id := uuid.Must(uuid.NewV4())
+	log.Printf("Notifier.Subscribe %s", id.String())
+	b.clients = append(b.clients, sucscription{id, r})
+	return id
 }
 
-func (b *Notifier) Unsubscribe(i int) {
-	if i > len(b.clients) {
-		return
+func (b *Notifier) Unsubscribe(id uuid.UUID) {
+	log.Printf("Notifier.Unsubscribe %s", id.String())
+	clients := make([]sucscription, 0)
+	for _, s := range b.clients {
+		if !uuid.Equal(id, s.id) {
+			clients = append(clients, s)
+		}
 	}
-	head := b.clients[:i]
-	if i2 := i + 1; i2 < len(b.clients) {
-		tail := b.clients[i2:]
-		head = append(head, tail...)
-	}
-	b.clients = head
+	b.clients = clients
 }
